2c11713b93ff0ecc8f393f6a121bd4a29ed4683f
xiaofxuan <1377926472@qq.com>
2014-03-09 16:02:10 +0800

    完成注册功能

--- views/master_view.php
+++ views/master_view.php
@@ -1,7 +1,17 @@
 <!DOCTYPE HTML> <html>
     <head>
         <title><?php echo "MySYSU" ?></title>
-		
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <link href="./front_end/css/bootstrap.min.css" type="text/css" rel="stylesheet" media="screen"/> 
+        <!--<link href="<?php echo $cssfile ?>" type="text/css" rel="stylesheet" />
+
+        <link href="../front_end/css/bootstrap.css" type="text/css" rel="stylesheet" />
+        <link href="../front_end/css/jquery-ui-1.9.2.custom.css" type="text/css" rel="stylesheet" />       
+        <link href="../front_end/css/docs.css" type="text/css" rel="stylesheet" />
+        <link href="< ?php echo $cssfile2 ?>" type="text/css" rel="stylesheet" />-->
+        
+        <!--<script src="../front_end/js/bootstrap.js"></script>-->
+        <script src="./front_end/js/jquery.js"></script>
     </head>
     <body>
 <!--
@@ -20,5 +30,9 @@
                 2014 &copy; All rights reserved.          
             </div>
         </div>
+		<!--<script src="http://code.jquery.com/jquery.js"></script>-->
+        <script src="./front_end/js/bootstrap.min.js"></script>
+        <script src="<?php echo $jsfile ?>" type="text/javascript"></script>
+
     </body>
 </html>
