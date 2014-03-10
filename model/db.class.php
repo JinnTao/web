50af4ce65be19768641d4813e0926da1d50eb06e
JinnTao <314315805@qq.com>
2014-03-10 13:50:17 +0800

    update datebase
    increase function::del user del topic new user new topic
    check_password_email

--- model/db.class.php
+++ model/db.class.php
@@ -26,59 +26,180 @@ public function __construct() {
 private function __clone(){
 }
 
-
-public function insert_new_user($para){
+// insert new user!
+public function new_user($para){
 	try {
 		self::$instance->beginTransaction();
 		
-        $date = getdate();
-		$id = $date[0];
-	
+        $date = date('y-m-d');
+		
 		$para2 = array();
-		$para2[] = "'" . $id . "'"; // here calculate id.
 		$para2[] = "'" . $para['email'] . "'";
 		$para2[] = "'" . $para['password'] . "'";
-	        		
+	    $para2[] = "'" . $date . "'";
+				
 		$insert_string = implode(", ", $para2); 
-		$insert_string2 = "INSERT INTO users (uid, uemail, npwd)
+		$insert_string2 = "INSERT INTO users (email, password,sign_up_time)
 		" . " VALUES (" . $insert_string . " )";
 		self::$instance->exec($insert_string2);
 		self::$instance->commit();
-        return $id;
+        return $para['email'];
 		}
 	 catch (Exception $e) {
 		self::$instance->rollBack();
-		echo "cannot insert users   ".$e->getMessage(). "    " . $insert_string2;
+		echo "cannot insert users   ".$e->getMessage(). "    <br>"; 
 		return false;
 	}
 } 
 
+public function update_user($para){
+	try {
+		self::$instance->beginTransaction();
+		
+        $date = date('y-m-d');
+		
+		$para2 = array();
+		$para2[] = "'" . $para['email'] . "'";
+		$para2[] = "'" . $para['password'] . "'";
+	    $para2[] = "'" . $date . "'";
+				
+		$insert_string = implode(", ", $para2); 
+		$insert_string2 = "INSERT INTO users (email, password,sign_up_time)
+		" . " VALUES (" . $insert_string . " )";
+		self::$instance->exec($insert_string2);
+		self::$instance->commit();
+        return $para['email'];
+		}
+	 catch (Exception $e) {
+		self::$instance->rollBack();
+		echo "cannot insert users   ".$e->getMessage(). "    <br>"; 
+		return false;
+	}
+} 
+
+// test email wether email exist!
 function has_user_email($email){
 	try{
 		self::$instance->beginTransaction();
-		$check_string = "Select uid From Users Where uemail = " . "'" . $email . "'";
+		$check_string = "Select id From Users Where email = " . "'" . $email . "'";
 		$query = self::$instance->query($check_string);
 		self::$instance->commit();
 		$query->setFetchMode(PDO::FETCH_ASSOC); 
 		$result = $query->fetchAll();
 		if($result){
-			// echo "exist";
+			//echo "exist <br>";
 			return true;
 		}
 		else{
-			// echo "unexist";
+			//echo "unexist <br>";
 			return false;
 		}
 
 	}
 	 catch (Exception $e) {
 		 self::$instance->rollBack();
-		 echo "cannot check email ".$e->getMessage(). "    " . $insert_string2;
+		 echo "cannot check email ".$e->getMessage(). "    <br>";
 		 return false;
 	}
 }
 
+// check email and password 2 correct 1 password incorrect 0 email unexist
+function check_email_password($email,$password){
+	try{
+		self::$instance->beginTransaction();
+		$check_string = "Select password From Users Where email = " . "'" . $email . "'";
+		$query = self::$instance->query($check_string);
+		self::$instance->commit();
+		$query->setFetchMode(PDO::FETCH_ASSOC); 
+		$result = $query->fetchAll();
+		//print_r($result[0]['password']);
+		
+		if($result){
+			if($result[0]['password'] === $password){
+				//echo "you are in  <br>";
+				return 2;
+			}
+			else{
+				//echo "password incorrect! <br>";
+				return 1;
+			}
+		}
+		else{
+			//echo "email unexist <br>";
+			return 0;
+		}
 
+	}
+	 catch (Exception $e) {
+		 self::$instance->rollBack();
+		 echo "Error: ".$e->getMessage(). "    <br>" ;
+		 return false;
+	}
+}
+
+public function new_topic($para){
+	try {
+		// test para valign?
+		if(empty($para['title']) or empty($para['content'])){
+			echo 'title or content is empty <br>';
+			return false;
+		}
+		
+		//begin insert
+		self::$instance->beginTransaction();
+        $date = date('y-m-d');
+	
+		$para2 = array();
+		$para2[] = "'" . $para['title'] . "'";
+		$para2[] = "'" . $para['content'] . "'";
+		$para2[] = "'" . $para['start_id'] . "'";
+	    $para2[] = "'" . $date . "'";
+				
+		$insert_string = implode(", ", $para2); 
+		$insert_string2 = "INSERT INTO topics (title,content,start_id,start_time)
+		" . " VALUES (" . $insert_string . " )";
+		self::$instance->exec($insert_string2);
+		self::$instance->commit();
+        //echo "topic create successfully!";
+		return true;
+		}
+	 catch (Exception $e) {
+		self::$instance->rollBack();
+		echo "cannot create topics   ".$e->getMessage(). "    <br>"; 
+		return false;
+	}
+} 
+public function del_topic($id){
+	try {
+		self::$instance->beginTransaction();
+		$insert_string2 = "DELETE FROM topics WHERE id = '".$id."'";
+		self::$instance->exec($insert_string2);
+		self::$instance->commit();
+        echo "topic del successfully!";
+		return true;
+		}
+	 catch (Exception $e) {
+		self::$instance->rollBack();
+		echo "cannot del ".$e->getMessage(). "    <br>"; 
+		return false;
+	}
+} 
+
+public function del_user($id){
+	try {
+		self::$instance->beginTransaction();
+		$insert_string2 = "DELETE FROM Users WHERE id = '".$id."'";
+		self::$instance->exec($insert_string2);
+		self::$instance->commit();
+        echo "User del successfully!";
+		return true;
+		}
+	 catch (Exception $e) {
+		self::$instance->rollBack();
+		echo "cannot del ".$e->getMessage(). "    <br>"; 
+		return false;
+	}
+} 
 
 
 }/*** end of class ***/
--- model/initial.db.php
+++ model/initial.db.php
@@ -15,14 +15,20 @@
 	}
 	mysql_close($link);
 	
-	/*create tables*/
+	/*create tables Users*/
 	$link = mysql_connect($db_config["hostname"], $db_config["username"], $db_config["password"]);
 	mysql_select_db($db_config["database"], $link);
 	$sql_create_user_table = "CREATE TABLE Users(
-	uid CHAR(10),
-	uemail VARCHAR(20),
-	npwd VARCHAR(10),
-	PRIMARY KEY (uid))";
+	id int(10) NOT NULL auto_increment,
+	email VARCHAR(20) NOT NULL,
+	password VARCHAR(20) NOT NULL,
+	name VARCHAR(15),
+	photo VARCHAR(20),
+	gender CHAR(1),
+	age CHAR(4),
+	description VARCHAR(50),
+	sign_up_time DATE NOT NULL,
+	PRIMARY KEY (id))";
 	
 	if(mysql_query($sql_create_user_table, $link)){
 		echo 'Table Users created!' . "<br>";
@@ -33,6 +39,56 @@
 		
 	mysql_close($link);
 	
+	/*create tables Admin*/
+	$link = mysql_connect($db_config["hostname"], $db_config["username"], $db_config["password"]);
+	mysql_select_db($db_config["database"], $link);
+	$sql_create_table = "CREATE TABLE Admin(
+	id int(10) NOT NULL auto_increment,
+	name VARCHAR(20) NOT NULL,
+	password VARCHAR(20) NOT NULL,
+	description VARCHAR(50),
+	PRIMARY KEY (id))";
+	
+	/*intialize admin account*/
+	if(mysql_query($sql_create_table, $link)){
+		echo 'Table Admin created!' . "<br>";
+	}
+	else
+		echo 'Error creating table::' . mysql_error() . "<br>";
+		
+	
+	$root_admin_sql =  "Insert Into Admin(name,password,description) VALUES('administration','5588','permission is all')";
+	if(mysql_query($root_admin_sql))
+	{
+		echo 'Admin root initialized! <br>';
+	}
+	else
+		echo 'Error initialized ::'.mysql_error().'<br>';
+		
+	
+	mysql_close($link);
+
+	/*create tables Topics*/
+	$link = mysql_connect($db_config["hostname"], $db_config["username"], $db_config["password"]);
+	mysql_select_db($db_config["database"], $link);
+	$sql_create_table = "CREATE TABLE Topics(
+	id int(11) NOT NULL auto_increment,
+	title VARCHAR(20) NOT NULL,
+	content VARCHAR(100) NOT NULL,
+	start_id int(10) NOT NULL,
+	start_time DATE NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (start_id) references Users (id) ON DELETE CASCADE
+	)";
+	
+	if(mysql_query($sql_create_table, $link)){
+		echo 'Table Topics created!' . "<br>";
+	}
+	else
+		echo 'Error creating table::' . mysql_error() . "<br>";
+		
+		
+	mysql_close($link);
 	
 ?>
 </html>
